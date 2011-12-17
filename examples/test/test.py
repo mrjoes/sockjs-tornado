d1612a016c4db9d4bb3da35e4d9cc1e5640ceb75
Serge S. Koval <serge.koval@gmail.com>
2011-12-18 01:02:16 +0200

    Extended test server to support all required handlers from qunit tests.

--- examples/test/test.py
+++ examples/test/test.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 
 from tornado import web, ioloop
 
@@ -18,20 +19,58 @@ class CloseConnection(SockJSConnection):
         pass
 
 
+class TickerConnection(SockJSConnection):
+    def on_open(self, info):
+        self.timeout = ioloop.PeriodicCallback(self._ticker, 1000)
+        self.timeout.start()
+
+    def on_close(self):
+        self.timeout.stop()
+
+    def _ticker(self):
+        self.send('tick!')
+
+
+class BroadcastConnection(SockJSConnection):
+    clients = set()
+
+    def on_open(self, info):
+        self.clients.add(self)
+
+    def on_message(self, msg):
+        self.broadcast(self.clients, msg)
+
+    def on_close(self):
+        self.clients.remove(self)
+
+
+class AmplifyConnection(SockJSConnection):
+    def on_message(self, msg):
+        n = math.floor(float(msg))
+        if n < 0 or n > 19:
+            n = 1
+
+        self.send('x' * (math.pow(2, n) + 1))
+
 if __name__ == '__main__':
     import logging
     logging.getLogger().setLevel(logging.DEBUG)
 
     EchoRouter = SockJSRouter(EchoConnection, '/echo')
-
-    CloseRouter = SockJSRouter(CloseConnection, '/close')
-
     WSOffRouter = SockJSRouter(EchoConnection, '/disabled_websocket_echo',
                             user_settings=dict(disabled_transports=['websocket']))
+    CloseRouter = SockJSRouter(CloseConnection, '/close')
+    TickerRouter = SockJSRouter(TickerConnection, '/ticker')
+    AmplifyRouter = SockJSRouter(AmplifyConnection, '/amplify')
+    BroadcastRouter = SockJSRouter(BroadcastConnection, '/broadcast')
 
     app = web.Application(EchoRouter.urls +
+                          WSOffRouter.urls +
                           CloseRouter.urls +
-                          WSOffRouter.urls)
+                          TickerRouter.urls +
+                          AmplifyRouter.urls +
+                          BroadcastRouter.urls
+                          )
 
     app.listen(8080)
     ioloop.IOLoop.instance().start()
