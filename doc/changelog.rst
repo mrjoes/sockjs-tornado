ea12d149c703538ed10329db3f75bc224b53c1b3
Serge S. Koval <serge.koval@gmail.com>
2012-04-03 14:38:40 +0300

    Bumped version and added changelog.

---
+++ doc/changelog.rst
@@ -0,0 +1,19 @@
+Changelog
+---------
+
+0.0.3
+~~~~~
+
+ - SockJS 0.3 support
+ - Connection multiplexing sample
+ - IP validation is now optional
+
+0.0.2
+~~~~~
+
+ - SockJS 0.2 support and compatibility fixes
+
+0.0.1
+~~~~~
+
+Initial release.
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 
 try:
     from setuptools import setup, find_packages
@@ -12,14 +13,21 @@ try:
 except:
     license = None
 
-try:
-    readme = open('README.md').read()
-except:
-    readme = None
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def desc():
+    info = read('README.rst')
+    try:
+        return info + '\n\n' + read('doc/changelog.rst')
+    except IOError:
+        return info
 
 setup(
     name='sockjs-tornado',
-    version='0.0.2',
+    version='0.0.3',
     author='Serge S. Koval',
     author_email='serge.koval@gmail.com',
     packages=['sockjs', 'sockjs.tornado', 'sockjs.tornado.transports'],
@@ -28,7 +36,7 @@ setup(
     url='http://github.com/MrJoes/sockjs-tornado/',
     license=license,
     description='SockJS python server implementation on top of Tornado framework',
-    long_description=readme,
+    long_description=desc(),
     requires=['tornado'],
     install_requires=[
         'tornado >= 2.1.1'
